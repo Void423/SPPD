9abf0b1ccfd2d1149c3ececfe1d4011c40b77c89
Daffa Rihadatul Fairus <hanura423@gmail.com>
2026-05-04 22:45:52 +0700

    feat : make user table

--- src/db/schema/user.schema.ts
+++ src/db/schema/user.schema.ts
@@ -1,16 +1,41 @@
-import { integer, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
+import {
+  integer,
+  pgEnum,
+  pgTable,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
 import { createInsertSchema } from "drizzle-zod";
 import z from "zod";
 
+const roleEnum = pgEnum("role", ["admin", "karyawan"]);
+const positionEnum = pgEnum("position", [
+  "tendik",
+  "guruKelas",
+  "guruBidangStudi",
+  "kepsek",
+  "cleaningServices",
+  " penjagaSekolah",
+]);
 export const usersTable = pgTable("users", {
-   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-   name: varchar().notNull(),
-   email: varchar().notNull().unique(),
-   createdAt: timestamp().defaultNow().notNull(),
-   updatedAt: timestamp().defaultNow().notNull(),
+  id: integer().primaryKey().generatedByDefaultAsIdentity(),
+  name: varchar().notNull(),
+  email: varchar().notNull().unique(),
+  nip: varchar().notNull().unique(),
+  rank: varchar().notNull(),
+  role: roleEnum("role"),
+  position: positionEnum("position"),
+  createdAt: timestamp().defaultNow().notNull(),
+  updatedAt: timestamp()
+    .defaultNow()
+    .notNull()
+    .$onUpdate(() => new Date()),
 });
 
-export const insertSchema = createInsertSchema(usersTable, {
-   name: z.string().min(1, "Please insert a name"),
-   email: z.email(),
+export const userInsertSchema = createInsertSchema(usersTable).omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
 });
+
+export type userInput = z.infer<typeof userInsertSchema>;
