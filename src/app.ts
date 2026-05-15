6ecbaa453ddebb7bc9f54ae552ecd4317c39aa59
Daffa Rihadatul Fairus <hanura423@gmail.com>
2026-05-15 21:38:23 +0700

    feat: initialize Express server with Drizzle ORM and Better Auth configuration

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    origin: true,
+}))
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
+export default app;
